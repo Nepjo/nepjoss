0c7ffc9a02682f093eab6aea58a7160184370090
Vova <nepjo@mail.ru>
2019-09-02 13:55:58 +0300

    Musorka

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var money, time, appData;
-money = prompt("Ваш бюджет на месяц?");
+money = +prompt("Ваш бюджет на месяц?");
 time = prompt("Введите дату в формате YYYY-MM-DD");
 appData = {
     budget: money,
@@ -13,8 +13,22 @@ var exp, value;
 for(i=0; i<2; i++){
     exp = prompt("Введите обязательную статью расходов в этом месяце");
     value = prompt("Во сколько обойдется?");
-    appData.expenses+= exp + " : " + value + '\n';
+    if(typeof(exp) === String && typeof(exp) != null && typeof(value) != null && exp != '' && value != ''){
+        appData.expenses[exp] = value;
+    }
+    
 }
-alert(appData.budget/30);
-console.log(appData.expenses);
+appData.moneyPday = appData.budget/30;
+if(appData.moneyPday < 100){
+    alert('БЕГИ ОТСЮДА');
+}else if(appData.moneyPday < 200 && appData.moneyPday > 100){
+    alert('БЕГИ ОТСЮДа ээ');
+}else if(appData.moneyPday < 400 && appData.moneyPday > 300){
+    alert('БЕГИ ОТСЮДа ээ дурак шоле');
+}else if(appData.moneyPday > 1000){
+    alert('Тупа дурак');
+}else{
+    alert('Ты цифры вводи дуреньб');
+}
+
     
